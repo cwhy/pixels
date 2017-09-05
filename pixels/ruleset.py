f1a256e383dc35cc2db17b1ab254c227d4ee0975
Chen Yu <chenyu.nus@gmail.com>
2017-09-05 18:51:46 +0800

    first commit

---
+++ pixels/ruleset.py
@@ -0,0 +1,2 @@
+class RuleSet:
+    def __init__(self, config=None, n_order=None, n_dim=None):
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pixels',
+    version='0.1dev',
+    packages=['pixels'],
+    url='',
+    license='',
+    author='Chen Yu',
+    author_email='chenyu.nus@gmail.com',
+    description='A series of logical visual tasks for machine learning benchmarking'
+)
